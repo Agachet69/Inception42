27f4232e4e103bb19da1838d10fbf749d63fc62b
alexis gachet <gachet.alexisfc@gmail.com>
2023-01-10 04:44:21 +0100

    inception presque fonctionnel

---
+++ srcs/mariadb/tools/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Editing ports for config files
+sed -ie 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+sed -ie 's/#port/port/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+#Start mysql and create db if it doesn't exist
+if [ ! -d /var/lib/mysql/DB ]
+then
+service mysql start
+apt-get install -y gettext-base
+envsubst < /etc//mysql/db.sql | mysql
+mysqladmin -u root password $DB_ROOT_PASSWORD
+service mysql stop
+fi
+chown -R mysql:mysql /var/lib/mysql/
+
+#Running mysql
+mysqld_safe
